ae26318c47d94a259c1ea26f6d89976dfb2f4b34
Code <sangw009@umn.edu>
2022-09-19 20:18:25 -0500

    user character selection function, need to create a for loop and bug fix

--- Develop/script.js
+++ Develop/script.js
@@ -18,65 +18,40 @@ function passOptions() {
 }
 
 function charOptions() {
-  var characterOp = confirm("Please choose characters to use: lowercase, uppercase, numeric, special characters. [Minimum 2]");
-  if (characterOp = true){
-    confirm("Use Lowercase characters?");
-    characterOp = ("qwertyuiopasdfghjklzxcvbnm");
-    if (characterOp = ("qwertyuiopasdfghjklzxcvbnm"));
-    confirm("Use Uppercase characters?");
-    if (characterOp = true){
-      characterOp = ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM");
-      if (characterOp = ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"));
-      confirm("Use numeric characters?");
+  var lowercaseChar = ("qwertyuiopasdfghjklzxcvbnm")
+  var uppercaseChar = ("QWERTYUIOPASDFGHJKLZXCVBNM")
+  var numericChar = ("12345679")
+  var specialChar = ("!@#$%^&*()")
+  var allChar = ("")
 
-      if ( characterOp = false){
-        characterOp = ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM")
+  if (confirm("Please choose characters to use: lowercase, uppercase, numeric, special characters.")) {
+    if (confirm("Use lowercase letters?")) {
+      allChar1 = allChar.concat(lowercaseChar);
+      if (confirm("Use uppercase?")) {
+        allChar1 = allChar.concat(uppercaseChar, lowercaseChar);
+        if (confirm("use numeric?")) {
+          allChar1 = allChar.concat(numericChar, uppercaseChar, lowercaseChar);
+          if (confirm("use special?")) {
+            allChar1 = allChar.concat(specialChar, numericChar, uppercaseChar, lowercaseChar);
+          } else {
+            allChar1 = allChar.concat(numericChar, lowercaseChar, uppercaseChar);
+          }
+        } else {
+          allChar1 = allChar.concat(lowercaseChar, uppercaseChar);
+        }
+      } else {
+        allChar1 = allChar.concat(lowercaseChar);
       }
-
-      else if (characterOp = true){
-        characterOp = ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890");
-        confirm("Use special characters?");
-      }
-
-      if (characterOp = false){
-      characterOp = ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789")
-      }
-      
-      else if( characterOp = true){
-        characterOp = ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$&*")
-      }
-
+    } else {
+      allChar1 = ""
     }
+  } else {
+    window.alert("please say something")
+  }
+  console.log(allChar1)
+  return allChar1
+}
 
-  }
-console.log(characterOp);
-return characterOp;
-}
- /*
-  if (characterOp === "LUNS") {
-    window.alert("You have selcted lowercase, uppercase, numeric, and special characters");
-    characterOp = ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM123456789!@#$%^&*");
-  }
-  else if (characterOp === "LUN") {
-    window.alert("You have selcted lowercase, uppercase, and numeric characters");
-    characterOp = ("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890");
-  }
-  else if (characterOp === "UNS") {
-    window.alert("You have selected uppercase, numeric, and special characters");
-    characterOp = ("QWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()");
-  }
-  else if (characterOp === "LNS") {
-    window.alert("You have slected lowercase, numeric, and special characters");
-    characterOp = ("qwertyuiopasdfghjklzxcvbnm1234567890!@#$%^&*()");
-  }
-  else if (characterOp != "LUNS") {
-    window.alert("Please select valid character!");
-    characterOp = ("undefined");
-  }
-  console.log(characterOp);
-  return characterOp;
-}
-*/
 // Password generator function 
 function generatePassword() {
   var passLength = passOptions();
